4cf32474a6f62c0dc0903a7568116d897b9a7a6d
Alone_X <mandarjoshi079@gmail.com>
2023-12-18 20:24:31 +0530

    Added DotEnv Package

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express'); // Require express module
 const bodyParser = require('body-parser'); // Require body-parser module
-
+require('dotenv').config() // Require dotenv file
 const app = express(); // Create express app
-const port = process.env.PORT;  // Server port
+const port = process.env.PORT || 80;  // Server port
 app.use(express.static(__dirname)); // Serve static files from the current directory
 
 app.use(bodyParser.urlencoded({ extended: true })); // Use body-parser middleware
