258715b5b074ea9cbce7407d906f24582dbf9838
Alone_X <62175217+AloneX079@users.noreply.github.com>
2023-12-19 13:42:22 +0530

    Comment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express'); // Require express module
 const bodyParser = require('body-parser'); // Require body-parser module
-require('dotenv').config() // Require dotenv file
+require('dotenv').config() // Fetches DotEnv Package to access dotenv file
 const app = express(); // Create express app
 const port = process.env.PORT || 80;  // Server port
 app.use(express.static(__dirname)); // Serve static files from the current directory
