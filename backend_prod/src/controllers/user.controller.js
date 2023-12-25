557e7f908e6908493fdf95d66ec94ba9520444bd
Alone_X <mandarjoshi079@gmail.com>
2023-12-25 15:23:28 +0530

    bits

--- backend_prod/src/controllers/user.controller.js
+++ backend_prod/src/controllers/user.controller.js
@@ -1,9 +1,14 @@
+const { application } = require('express');
 const asyncHandler = require('../utils/asyncHandler');
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "Ok"
-    })
+    const {name,email,phone,password} = req.body
+    console.log(name,email,phone,password)
+    if([name,email,phone,password].some((field)=>field?.trim() === "")){
+        throw new apierror(400,"Please fill all the fields!")
+    }
 })
+
+
 
 module.exports = {registerUser}
--- backend_prod/src/models/user.model.js
+++ backend_prod/src/models/user.model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
     uid:{
