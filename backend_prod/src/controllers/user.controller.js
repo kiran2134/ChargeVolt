0cbf93b09791264e1b1f41f43028ae4426633409
Alone_X <mandarjoshi079@gmail.com>
2023-12-25 15:55:56 +0530

    Validation

--- backend_prod/src/controllers/user.controller.js
+++ backend_prod/src/controllers/user.controller.js
@@ -1,12 +1,32 @@
-const { application } = require('express');
-const asyncHandler = require('../utils/asyncHandler');
+const user=require('../models/user.model.js');
+const asyncHandler = require('../utils/asyncHandler.js');
+const apierror = require('../utils/apierror.js');
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+
 
 const registerUser = asyncHandler(async (req, res) => {
     const {name,email,phone,password} = req.body
-    console.log(name,email,phone,password)
+    
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const phoneRegex = /^\d{10}$/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/;
+
     if([name,email,phone,password].some((field)=>field?.trim() === "")){
         throw new apierror(400,"Please fill all the fields!")
     }
+    if(emailRegex.test(email) === false){
+        throw new apierror(400,"Please enter a valid email!")
+    }
+    if(phoneRegex.test(phone) === false){
+        throw new apierror(400,"Please enter a valid phone number!")
+    }
+    if(passwordRegex.test(password) === false){
+        throw new apierror(400,"Please enter a valid password!")
+    }
+
+    console.log(name,email,phone,password)
 })
 
 
