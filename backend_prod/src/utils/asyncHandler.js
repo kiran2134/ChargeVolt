f7de7f1c317192906b8588a809c33e652bdc3d49
Anand  Sarolkar <98941524+Riseonelimit@users.noreply.github.com>
2024-01-08 15:46:08 +0530

    [FIXED] apierror

--- backend_prod/src/utils/asyncHandler.js
+++ backend_prod/src/utils/asyncHandler.js
@@ -3,7 +3,10 @@ const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).
         //if requestHandler returns a promise, resolve it
-        catch((err)=>next(err))
+        catch((err)=>{
+            res.status(err.statusCode).json(err)
+            next(err)
+        })
         //if requestHandler throws an error, pass it to next()
     }
 }
